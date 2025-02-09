5b9b1d883dbd1355c1550bd2ce1ecb2826fe19f7
kendall <kpfries@gmail.com>
2025-02-09 02:39:43 -0600

    add demo textures for height blending implement height blending node with bias and sharpness controls for fine tuning

--- Addons/ShaderNodes/height_blending.gd
+++ Addons/ShaderNodes/height_blending.gd
@@ -16,7 +16,11 @@ func _get_description():
 
 
 func _init():
-    set_input_port_default_value(2, 0.5)
+    set_input_port_default_value(0, 0.0)
+    set_input_port_default_value(1, 0.0)
+    set_input_port_default_value(2, 0.0)
+    set_input_port_default_value(3, 0.0)
+    set_input_port_default_value(4, 0.0)
     pass
 
 
@@ -26,7 +30,7 @@ func _get_return_icon_type():
 
 
 func _get_input_port_count():
-    return 3
+    return 4
 
 
 func _get_input_port_name(port):
@@ -36,7 +40,9 @@ func _get_input_port_name(port):
         1:
             return "b"
         2:
-            return "factor"
+            return "bias"
+        3:
+            return "sharpness"
 
 
 func _get_input_port_type(port):
@@ -46,6 +52,8 @@ func _get_input_port_type(port):
         1:
             return VisualShaderNode.PORT_TYPE_SCALAR
         2:
+            return VisualShaderNode.PORT_TYPE_SCALAR
+        3:
             return VisualShaderNode.PORT_TYPE_SCALAR
 
 
@@ -68,16 +76,18 @@ func _get_output_port_type(port):
 
 func _get_global_code(mode):
     return """
-        float height_blend() {
-            float blendval = 1.0;
-            return blendval;
-        }
+float height_blend(float a, float b, float bias, float sharpness) {
+    float delta = (-a + b) * 2.0 + 1.0;
+    float inv_sharp = (1.0 - sharpness) / 2.0;
+    float compensated_delta = clamp(delta + bias, 0.0, 1.0);
+    float compressed_delta = smoothstep(0.5 - inv_sharp, 0.5 + inv_sharp, compensated_delta);
+    return compressed_delta;
+}
     """
 
 
 func _get_code(input_vars, output_vars, mode, type):
     #return output_vars[0] + " = cnoise(vec3((%s.xy + %s.xy) * %s, %s)) * 0.5 + 0.5;" % [input_vars[0], input_vars[1], input_vars[2], input_vars[3]]
     var code = """
-
                 """
-    return code + output_vars[0] + """= height_blend();"""
+    return code + output_vars[0] + """= height_blend(%s, %s, %s, %s);""" % [input_vars[0], input_vars[1], input_vars[2], input_vars[3]]
